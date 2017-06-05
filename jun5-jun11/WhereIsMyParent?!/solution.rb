4ea0b01a54a8026a40218eca2eaf7f5398549307
nlau77 <nlau777@gmail.com>
2017-06-05 15:17:32 -0400

    monday where is my parent

---
+++ jun5-jun11/WhereIsMyParent?!/solution.rb
@@ -0,0 +1,9 @@
+# best solution from another use
+def find_children(brigade)
+  brigade.chars.sort_by{|letter| [letter.downcase, letter]}.join
+end
+
+# What I learned
+# sort_by can take multiple parameters
+# if the first parameter results in a tie, the second parameter will be used
+# if that results in a tie, a third parameter will be used (if inputed)
