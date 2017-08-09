01e2626e2ef7e3b3263a3ad5618222a6907cae2d
nlau77 <nlau777@gmail.com>
2017-08-09 11:42:11 -0400

    find the captials

---
+++ august/javascript/FindTheCaptials.js
@@ -0,0 +1,17 @@
+// question
+Find the index for each captial letter in string
+
+// example
+capitals("aPPle") -> [1, 2]
+capitals("WaterMeloN") -> [0, 5, 9]
+
+// solution
+var capitals = function (word) {
+  let capital_indexes = []
+  word.split("").forEach((letter, index) =>{
+    if (letter === letter.toUpperCase()){
+      capital_indexes.push(index)
+    }
+  })
+  return capital_indexes
+}
